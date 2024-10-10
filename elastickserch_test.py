5e2f9cff05c8e3964e92ea631cc4cfc34f8c8c2d
gomi <soredemo@outlook.jp>
2024-10-10 21:22:43 +0900

    add

--- elastickserch_test.py
+++ elastickserch_test.py
@@ -1,25 +1,29 @@
 '''
 pip install elasticsearch
 '''
-from elasticsearch import Elasticsearch
 import re
+from elasticsearch import Elasticsearch
 
 # Elasticsearchの接続設定
-es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+es = es = Elasticsearch("http://localhost:9200")
 
 # Apacheログファイルのパス
-log_file_path = '/path/to/access.log'
+log_file_path = 'test_input/access_log.log'  # ここを実際のログファイルのパスに変更してください
 
 # Apacheのアクセスログのパターン
 log_pattern = r'(?P<ip>[\d\.]+) - - \[(?P<date>.+?)\] "(?P<request>.+?)" (?P<status>\d{3}) (?P<size>\d+|-)'
 
-# ログファイルを読み込み、各行を解析してElasticsearchに登録
-with open(log_file_path) as log_file:
-    for line in log_file:
-        match = re.match(log_pattern, line)
-        if match:
-            log_entry = match.groupdict()
-            # Elasticsearchにデータを登録
-            es.index(index='apache_logs', document=log_entry)
+def main():
+    # ログファイルを読み込み、各行を解析してElasticsearchに登録
+    with open(log_file_path) as log_file:
+        for line in log_file:
+            match = re.match(log_pattern, line)
+            if match:
+                log_entry = match.groupdict()
+                # Elasticsearchにデータを登録
+                es.index(index='apache_logs', document=log_entry)
 
-print("ログの登録が完了しました。")
+    print("ログの登録が完了しました。")
+
+if __name__ == '__main__':
+    main()
